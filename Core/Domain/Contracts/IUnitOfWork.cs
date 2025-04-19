41c6cb11449d88864e76af06106c1bc23d766187
Mohamed Atef <Mohamed Atef>
2025-04-19 15:34:22 +0200

    Unit Of Work

---
+++ Core/Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsnc();
+
+        // Generate Repository
+        IGenericRepository<TEntity, Tkey> GetRepository<TEntity, Tkey>() where TEntity : BaseEntity<Tkey>;
+    }
+}
