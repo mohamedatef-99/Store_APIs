3b015a04cc9745a7bf356a45efa08129346c4f99
Mohamed Atef <Mohamed Atef>
2025-04-22 19:53:01 +0200

    PictureUrl using Funtion Resolver

---
+++ Core/Services/MappingProfiles/PictureUrlResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Models;
+using Microsoft.Extensions.Configuration;
+using Shared;
+
+namespace Services.MappingProfiles
+{
+    public class PictureUrlResolver(IConfiguration configuration) : IValueResolver<Product, ProductResultDto, string>
+    {
+        public string Resolve(Product source, ProductResultDto destination, string destMember, ResolutionContext context)
+        {
+            //source.PictureUrl = source.PictureUrl ?? string.Empty; // Check if PictureUrl is null
+            if (string.IsNullOrEmpty(source.PictureUrl)) return string.Empty; // Reutrn empty string if PictureUrl is null or empty
+            return $"{configuration["BaseUrl"]}/{source.PictureUrl}"; // Return the URL
+        }
+    }
+}
